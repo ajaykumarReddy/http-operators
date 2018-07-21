2805aaa5f292ab8f413875072513706f1f19dc42
ajay <ajaychagam@gmail.com>
2018-07-21 12:10:29 +0530

    http-operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { keepFresh, shareAndCache, retryExponentialBackoff } from 'http-operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  repos;
+  constructor(private http: HttpClient) {
+    const path = `https://api.github.com/search/repositories?q=angular`;
+    this.repos = this.http.get<any>(path).pipe(
+      map(res => res.items),
+      retryExponentialBackoff(3, 1000),
+      keepFresh(10 * 1000),
+      shareAndCache('myapp-repos'),
+    );
+  }
 }
